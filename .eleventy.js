69cf97c76342c9908ca5242fbe120a320d8877a4
Håkon Olsen <hakdo@outlook.com>
2022-08-24 06:28:58 +0200

    moving js to separate file

--- .eleventy.js
+++ .eleventy.js
@@ -3,9 +3,11 @@ const { DateTime } = require('luxon');
 module.exports = function (eleventyConfig) {
     // Copy the `css` directory to the output
     eleventyConfig.addPassthroughCopy('css');
+    eleventyConfig.addPassthroughCopy('js');
   
     // Watch the `css` directory for changes
     eleventyConfig.addWatchTarget('css');
+    eleventyConfig.addWatchTarget('js');
     eleventyConfig.addFilter('readableDate', (dateObj) => {
         return DateTime.fromJSDate(dateObj, { zone: 'Europe/Oslo' }).toLocaleString(
           DateTime.DATE_SHORT
